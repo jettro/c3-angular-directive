5a9a68ea5f31791af1c610e4ec379e5a0f31cdcf
jettro <jettro@gridshore.nl>
2015-05-06 15:03:26 +0200

    fix for chrome

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var graphApp = angular.module('graphApp', ['gridshore.c3js.chart']);
 
 graphApp.controller('GraphCtrl', function ($scope) {
 	$scope.clicked = {};
-	$scope.showClick = function(data,element) {
-		$scope.clicked = data;
+	$scope.showClick = function(theData) {
+		$scope.clicked = theData;
 	}
 });
