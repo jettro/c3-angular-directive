d2ec14eb64d42e5cfd06958dc13f2f62ef556c77
jettro <jettro@gridshore.nl>
2015-05-07 17:18:55 +0200

    More samples, now also for time based data

--- examples/assets/js/app.js
+++ examples/assets/js/app.js
@@ -1,9 +1,38 @@
-var graphApp = angular.module('graphApp', ['gridshore.c3js.chart']);
+var graphApp = angular.module('graphApp', ['gridshore.c3js.chart','graphApp.services']);
 
-graphApp.controller('GraphCtrl', function ($scope) {
+graphApp.controller('GraphCtrl', function ($scope,$interval,dataService) {
+    $scope.datapoints=[];
+    $scope.datacolumns=[{"id":"top-1","type":"line","name":"Top one"},
+        {"id":"top-2","type":"spline","name":"Top two"}];
+    $scope.datax={"id":"x"};
+
+    $scope.generateData = function() {
+        $interval(function(){
+            dataService.loadData(function(data){
+                $scope.datapoints.push(data);
+            });
+        },1000,10);
+    };
+
     $scope.clicked = {};
-
     $scope.showClick = function(data) {
         $scope.clicked = data;
     }
+});
+
+var services = angular.module('graphApp.services', []);
+services.factory('dataService', function() {
+    function DataService() {
+        var maxNumber = 200;
+
+        // API methods
+        this.loadData = function(callback) {
+            callback({"x":new Date(),"top-1":randomNumber(),"top-2":randomNumber()});
+        };
+
+        function randomNumber() {
+            return Math.floor((Math.random() * maxNumber) + 1);
+        }
+    }
+    return new DataService();
 });
