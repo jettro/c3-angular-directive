d4be6f500e2cf129aaa62790da00578cf1f69a3e
jettro <jettro@gridshore.nl>
2015-05-07 16:10:08 +0200

    change api

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var graphApp = angular.module('graphApp', ['gridshore.c3js.chart']);
 
 graphApp.controller('GraphCtrl', function ($scope) {
 	$scope.clicked = {};
-	$scope.showClick = function(theData) {
-		$scope.clicked = theData;
+	$scope.showClick = function(data) {
+		$scope.clicked = data;
 	}
 });
