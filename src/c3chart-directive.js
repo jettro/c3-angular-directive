099f0ebbc5d861d00c2d8bee0f5d77edab16d947
Hugo Freire <hfreire@exec.sh>
2015-09-27 13:12:01 +0200

    Added support for chart transition duration.

--- src/c3chart-directive.js
+++ src/c3chart-directive.js
@@ -169,8 +169,7 @@ function C3Chart ($timeout) {
             "chartData": "=chartData",
             "chartColumns": "=chartColumns",
             "chartX": "=chartX",
-            "callbackFunction": "&",
-            "transitionDuration": "=transitionDuration",
+            "callbackFunction": "&"
         },
         "template": "<div><div id='{{bindto}}'></div><div ng-transclude></div></div>",
         "replace": true,
