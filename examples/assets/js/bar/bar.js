c4011530e8493146c0fd72c783cf3d4b146e50a6
Jettro Coenradie <jettro@gridshore.nl>
2016-07-23 22:10:50 +0200

    Issue #88: Added an example to the bars example

--- examples/assets/js/bar/bar.js
+++ examples/assets/js/bar/bar.js
@@ -38,6 +38,17 @@
         vm.handleCallbackGroup = handleCallbackGroup;
         vm.toggleGroup = toggleGroup;
 
+        vm.datapoints = [
+            {"x": "one", "top-1": 10, "top-2": 12},
+            {"x": "two", "top-1": 11, "top-2": 13},
+            {"x": "three", "top-1": 12, "top-2": 14},
+            {"x": "four", "top-1": 13, "top-2": 15},
+            {"x": "five", "top-1": 14, "top-2": 16}
+        ];
+        vm.datacolumns = [{"id": "top-1", "type": "bar", "name": "Top one"},
+            {"id": "top-2", "type": "bar", "name": "Top two"}];
+        vm.datax = {"id": "x"};
+
         function handleCallbackGroup(chartObj) {
             vm.chartObj = chartObj;
         }
